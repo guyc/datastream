b2959233870fda318450e1ffe7d05f55565f70e3
Guy Carpenter <guy@clearwater.com.au>
2014-06-22 19:54:11 +1000

    Add callback handler.

--- client/python/datastream/listener.py
+++ client/python/datastream/listener.py
@@ -1,12 +1,21 @@
 from ws4py.client.threadedclient import WebSocketClient
+import json
 
 class Listener(WebSocketClient):
-    def __init__(self, url):
+    def __init__(self, url, eventHandler):
         super(Listener,self).__init__(url, protocols=['data-stream'])
+        self.eventHandler = eventHandler
 
     def closed(self, code, reason):
         print (("Closed down", code, reason))
 
     def received_message(self, message):
         print("received message")
-        print("=> %d %s" % (len(m), str(m)))
+        print("=> %d %s" % (len(message), str(message)))
+        try:
+            data = json.loads(str(message))
+            self.eventHandler(data)
+        except Exception as Ex:
+            print("Exception decoding message.")
+            print(Ex)
+
