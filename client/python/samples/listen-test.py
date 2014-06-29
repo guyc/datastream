905a5e33b40ff2ef6f26169b3146f60415e4c11c
Guy Carpenter <guy@clearwater.com.au>
2014-06-29 15:44:48 +1000

    handle disconnect/reconnect

--- client/python/samples/listen-test.py
+++ client/python/samples/listen-test.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import datastream.listener
+import time
 
 url = 'ws://barrine.clearwater.com.au:8001/test'
+retrySeconds = 5
 
 def valueChanged(values):
     print("valueChanged: %s" % (values))
 
-try:
-  ws = datastream.listener.Listener('ws://barrine.clearwater.com.au:8001/test', valueChanged)
-  ws.daemon = False
-  ws.connect()
-except KeyboardInterrupt:
-  ws.close()
+while 1:
+  try:
+    ws = datastream.listener.Listener('ws://iot.clearwater.com.au:8001/test/build%', valueChanged)
+    ws.daemon = False
+    ws.connect()
+    print("done with connect")
+  except KeyboardInterrupt:
+    print("keyboard interrupt")
+    ws.close()
+    exit(0)
+  except Exception as e:
+    print(e)
+  else:
+    print("wait forever")
+    ws.run_forever()
+
+  print("disconnected, sleeping...\n")
+  time.sleep(retrySeconds)
+  print("reconnecting.\n")
 
 
